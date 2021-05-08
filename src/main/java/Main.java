3a5e26b0d9c60209e4aaa1f0a0134b8b0c4026be
AlexxxKRSK <C3i9vic1!>
2021-05-08 22:21:24 +0700

    Added method for new records check up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,10 @@ public class Main {
     static List<Option> now = new ArrayList<>();
     static List<Option> previous = new ArrayList<>();
     static List<Option> previousDay = new ArrayList<>();
+    static List<Record> ri = new ArrayList<>();
+    static List<Record> riDay = new ArrayList<>();
+    static List<Record> others = new ArrayList<>();
+    static List<Record> othersDay = new ArrayList<>();
     static String url;
     static String pass;
     static String user;
@@ -32,28 +36,32 @@ public class Main {
         sourceFolder = rb.getString("sourceFolder");
         folder = Path.of(sourceFolder);
         file = Path.of(sourceFolder);
-
     }
 
     public static void main(String[] args) throws InterruptedException {
         while (true) {
+//          Get entry<fileTimeUpdate:fileName> for most fresh file
             var dt = getLatestFile(folder);
+//          On new update start analyzing
             if(dt.getKey().toMillis()>fileTime.toMillis() ) {
                 fileTime = dt.getKey();
                 System.out.println(dt.getKey() + "  ***  " + dt.getValue());
+//                readFile(file);
+//                updateDB("DROP TABLE IF EXISTS previous;" +
+//                        "CREATE TABLE previous AS TABLE now;");
+//                writeDB(now, "now");
+                checkDB(ri, others);
+                others.forEach(System.out::println);
+//                readDB("SELECT COUNT(*) FROM now");
 
-                if(!dt.getValue().equals(file) & !dt.getValue().toString().equals(sourceFolder)){
-                    file = dt.getValue();
-                    System.out.println("New File found=" + file);
-                    previousDay=now;
-                    writeDB(previousDay,"previousDay");
-                }
-                updateDB("DROP TABLE IF EXISTS previous;" +
-                        "ALTER TABLE now RENAME to previous;");
-                updateDB(newTableNow);
-                readFile(file);
-                writeDB(now, "now");
-                readDB("SELECT COUNT(*) FROM now");
+//                If new file created
+//                if(!dt.getValue().equals(file) & !dt.getValue().toString().equals(sourceFolder)){
+//                    file = dt.getValue();
+//                    System.out.println("New File found=" + file);
+//                    previousDay=new ArrayList<>(previous);
+//                    writeDB(previousDay,"previousDay");
+//                }
+
             }
             Thread.sleep(5000);
         }
@@ -92,6 +100,33 @@ public class Main {
         }
     }
 
+    static void checkDB(List<Record> riList, List<Record> othersList) {
+        riList.clear();
+        othersList.clear();
+//        Select new deals greater then 1 mln RUB and insert into "ri" table
+        String sql = "SELECT now.date, code, now.base, now.type, now.strike, now.expiry, " +
+                "(now.numberofcontracts-previous.numberofcontracts)*now.theoreticalprice/1000000 as prcntChange, " +
+                "CASE WHEN now.type = 'Call' THEN (now.strike + now.theoreticalPrice) ELSE (now.strike - now.theoreticalPrice) END as level," +
+                " now.theoreticalPrice, now.numberofcontracts as oiNow, previous.numberofcontracts as oiPrev, " +
+                "(now.numberofcontracts - previous.numberofcontracts)/ now.numberofcontracts*100 as oiChange " +
+                "FROM now JOIN previous USING (code) WHERE (now.numberofcontracts!=previous.numberofcontracts) " +
+                "AND (now.numberofcontracts-previous.numberofcontracts)*now.theoreticalprice/1000000>=1;";
+
+        try (Connection conn = DriverManager.getConnection(url, user, pass)){
+            var statement = conn.prepareStatement(sql);
+            var rs = statement.executeQuery();
+            while (rs.next()){
+                if (rs.getString("code").startsWith("RI"))
+                    riList.add(new Record(rs));
+                else othersList.add(new Record(rs));
+           }
+            riList.forEach(x -> System.out.println("RI=" + x));
+            othersList.forEach(x -> System.out.println("Other=" + x));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     static void updateDB(String sql) {
         try (Connection conn = DriverManager.getConnection(url, user, pass)){
             var statement = conn.prepareStatement(sql);
@@ -103,7 +138,8 @@ public class Main {
 
     static void writeDB(List<Option> list, String table) {
         try (Connection conn = DriverManager.getConnection(url, user, pass)){
-            String sql = "INSERT INTO " + table + " VALUES (?,?,?,?,?, ?,?,?,?,?, ?,?,?) ON CONFLICT DO NOTHING";
+            String sql = "TRUNCATE " + table + ";" +
+                    "INSERT INTO " + table + " VALUES (?,?,?,?,?, ?,?,?,?,?, ?,?,?) ON CONFLICT DO NOTHING";
             for (Option opt: list){
                 var statement = conn.prepareStatement(sql);
                 statement.setDate(1, Date.valueOf(opt.getDt()));
@@ -130,12 +166,11 @@ public class Main {
         now.clear();
         try (var stream = Files.lines(path)) {
             stream.skip(1)
-//                    .limit(1)
+//                    .limit(100)
                     .map(s-> Arrays.asList(s.split(";")))
                     .forEach(l->now.add(new Option(l)));
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        System.out.println(now.size());
     }
 }
